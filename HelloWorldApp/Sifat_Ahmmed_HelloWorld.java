8c62832aa7ff4db34a49e3693b0f4a0634d2fb20
Sifat Ahmmed <sifat_ahmmed@hotmail.com>
2025-03-14 23:46:16 -0500

    Added HelloWorld Java program for Module 1

---
+++ HelloWorldApp/Sifat_Ahmmed_HelloWorld.java
@@ -0,0 +1,7 @@
+public class Sifat_Ahmmed_HelloWorld {
+    public static void main(String[] args) {
+        String firstName = "Sifat";
+        String lastName = "Ahmmed";
+        System.out.printf("Hello World from %s %s.%n", firstName, lastName);
+    }
+}
