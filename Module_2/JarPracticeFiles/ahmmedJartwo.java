f0c618cea2c2d8459f9e2a718878df0b81331b34
Sifat Ahmmed <sifat_ahmmed@hotmail.com>
2025-03-19 00:30:31 -0500

    Added Module 2 JAR practice files

---
+++ Module_2/JarPracticeFiles/ahmmedJartwo.java
@@ -0,0 +1,7 @@
+package Module_2.JarPracticeFiles;
+
+public class ahmmedJartwo {
+    public static void printMessage() {
+        System.out.println("Hello from ahmmedjartwo!");
+    }
+}
---
+++ Module_2/JarPracticeFiles/ahmmedjarone.java
@@ -0,0 +1,8 @@
+package Module_2.JarPracticeFiles;
+
+public class ahmmedjarone {
+    public static void main(String[] args) {
+        System.out.println("Hello from ahmmedjarone!");
+        ahmmedJartwo.printMessage();
+    }
+}
